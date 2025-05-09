eb7315bdd8f7d996563ff94a0c8925cff02a0066
Naeem Shaikh <naeem.shaikh@contentstack.com>
2025-05-09 19:38:56 +0530

    log

--- app/api/latency-within-region/route.js
+++ app/api/latency-within-region/route.js
@@ -4,9 +4,12 @@ export const dynamic = 'force-dynamic';
 export async function GET(request) {
 
     try{
+
+        const startTime = Date.now();
         const response = await fetch("https://latency-check.eu-gcpcontentstackapps.com/api/hello");
+        const endTime = Date.now();
         const body= await response.text()
-        console.log(body)
+        console.log("request - response time from region", endTime-startTime)
         return new Response(body, {
             status: 200,
             headers: {
